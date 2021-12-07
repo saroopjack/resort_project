c1dd19ebaf79f8ae278b8235774cf9d0fd7ce48c
saroopinfocruise <saroop@infocruise.in>
2021-12-07 14:24:14 +0530

    7/11/2021

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css'
+import Home from './pages/Home'
+import SingleRoom from './pages/SingleRoom'
+import Rooms from './pages/Rooms'
+import Error from './pages/Error'
+
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/rooms' element={<Rooms />} />
+        <Route exact path='/rooms/:slug' element={<SingleRoom />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
+import Services from '../components/Services'
+import FeaturecRooms from '../components/FeaturecRooms'
+
+const Home = () => {
+  return (
+    <>
+      <Hero>
+        <Banner
+          title='luxurious rooms'
+          subtitle='deluxe rooms starting at $299'
+        >
+          <Link to='/rooms' className='btn-primary'>
+            Our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+      <FeaturecRooms />
+    </>
+  )
+}
+
+export default Home
